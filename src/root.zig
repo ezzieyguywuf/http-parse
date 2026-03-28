2cf85e1a948a971fc7997d72eddbcc7cab6f3edb
Wolfgang E. Sanyer <wolfgangesanyer@gmail.com>
2026-03-27 23:11:54 -0400

    Remove most comments from zig init files

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,3 @@
-//! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
 const Io = std.Io;
 
@@ -6,13 +5,5 @@ const Io = std.Io;
 ///
 /// Accepting an `Io.Writer` instance is a handy way to write reusable code.
 pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
-}
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+    try writer.print("Hello http parse\n", .{});
 }
